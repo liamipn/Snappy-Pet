579fc942a469f442b5b425013a29fe0d6a78505d
liamipn <113452896+liamipn@users.noreply.github.com>
2022-09-22 12:42:27 +0100

    Css

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
     './templates/customers/*.liquid',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sp-green-1': '#56700f',
+        'sp-green-2': '#43570e',
+        'sp-orange-1': '#d5732f',
+        'sp-beige-1': '#f1ece7',
+      },
+    },
   },
   plugins: [],
 }
